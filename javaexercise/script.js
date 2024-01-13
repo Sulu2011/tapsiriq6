551e68e2800bbd7a8af2ec5cc78ae1dd60cc8770
Sulu2011 <126379528+Sulu2011@users.noreply.github.com>
2024-01-13 19:18:43 +0400

    Add files via upload

---
+++ javaexercise/script.js
@@ -0,0 +1,34 @@
+
+//document.body.classList.add('a');
+//let x = document.getElementsByTagName('li')
+//for (let index = 0; index <x.length; index++) {
+//x[index].style.color='green'
+//} birinci tapsiriq bitdi
+//ikinci tapsiriq bitmisdir
+
+//for (let index = 0; index <100; index++) {
+
+//if(index%2==0&&index%3==0){
+//    console.log(`${index} eded sade deyil`)
+
+//}
+//}
+//bele eleye bildim 3cu tapsiriq bitmisdir
+//let x=document.getElementsByTagName('div')
+//function random() {
+//    let r='0123456789ABCDEF'
+//    
+  //  for (let i = 0; i <6; i++) {
+ //       color += letters[Math.floor(Math.random() * 16)];
+ //   }
+   // return c;//muellim burda bu funksiyaya chatgptden baxdim ancaq bu hissesine. yeni bu hissesin ozum tapmadim
+//}
+//function reng(){
+//    let adam=document.getElementsByClassName('adam');
+//    x.style.color=random();
+//}
+//reng();
+//setInterval(function(){
+//    reng();
+//},2000); 4cu tapsiriq bitmisdir
+//5i yaza bilmedim
